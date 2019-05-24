cbc6f5f8729eb2fe3a725ac2d146657ed8097384
meng.tianxiang <meng.tianxiang@rongzer.com>
2019-05-24 14:29:06 +0800

    1

---
+++ my-vue/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- html -->
+  <div class="app">
+      <Hello :msg="msg" @on-emit="receive"></Hello>
+      <!-- 1234 -->
+      <button @click="btn1">按钮1</button> 
+      <!-- 5678 -->
+      <button @click="btn2">按钮2</button>
+  </div>
+</template>
+
+<script>
+  // js
+  import Hello from './components/HelloWorld'
+  export default {
+    components: {
+        Hello
+    },
+    data() {
+        return {
+            msg: ''
+        }
+    },
+    methods: {
+        btn1() {
+            this.msg = 1234
+        },
+        btn2() {
+            this.msg = 5678
+        },
+        receive(v) {
+            console.log('父组件的方法',v);
+        }
+    }
+  }
+</script>
+
+<style>
+/* css */
+</style>
---
+++ my-vue/src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+      <!-- <div v-for="(item, i) in arr" :key="i">{{item.b}}</div> -->
+      <!-- <div v-if="show">{{msg}}</div>
+      <div v-show="show">{{msg}}</div> -->
+      <input type="text" v-model="input">
+      <button @click="send" v-text="msg"></button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['msg'],
+    data() {
+      return {
+        arr: [
+          {a:1, b:2},
+          {a:11, b:22},
+          {a:111, b:222}
+        ],
+        show: false,
+        input: '初始的值1'
+      }
+    },
+    methods: {
+      send() {
+        this.$emit('on-emit', this.input)
+      }
+    },
+  }
+</script>
